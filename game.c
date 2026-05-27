ed3e5595d3d75def647a09f17f4c7133be9fabc9
Christ24x <christopheralois94@gmail.com>
2026-05-27 10:11:09 +0100

    Update game.c

---
+++ game.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "game.h"
+
+void afficherBienvenue()
+{
+    printf("====================================\n");
+    printf("      LE MOT LE PLUS LONG\n");
+    printf("====================================\n\n");
+}
+
+void afficherFinPartie()
+{
+    printf("\n====================================\n");
+    printf("            FIN DE PARTIE\n");
+    printf("====================================\n");
+}
